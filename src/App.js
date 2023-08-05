f9dedef6dec61505d0d5f37679c614d500354a4e
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-05 12:39:34 -0400

    Installed essential packages for styling and created files for the layout of the homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      "Test"
-    </div>
-  );
+  return <>
+  
+  </>;
 }
 
 export default App;
