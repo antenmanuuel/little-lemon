f85dfd65e710957a111de33ff501b2b9613bd26e
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-10 20:01:42 -0400

    add required assets for Home Page and setup layout for the Home Page

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,17 @@
+import WeekSpecials from './WeekSpecials';
+import Testimonials from './Testimonials';
+import OurStory from './OurStory';
+import Hero from './Hero';
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <WeekSpecials />
+            <Testimonials />
+            <OurStory />
+        </>
+    );
+}
+
+export default Home;
