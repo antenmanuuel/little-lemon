eabfb27773d5f97942a7db5123b70a49c1bc00a0
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-11 21:11:42 -0400

    Fix Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,50 +1,46 @@
-import { useState } from "react";
-import { useLocation, Link } from "react-router-dom";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import imageLogo from "./assets/logo.png";
-import "./css/Header.css";
-import links from "../../utils/links";
+import { useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import imageLogo from './assets/logo.png';
+import './css/Header.css';
+import links from '../../utils/links';
 
-const navLinks = Array.from(links.values()).filter(
-    (link) => link.anchorable
-  );
-  
+const navLinks = Array.from(links.values()).filter(link => link.anchorable);
+
 const Header = () => {
-  const [pathName] = useLocation();
+  const { pathName } = useLocation();
   const [navExpanded, setNavExpanded] = useState(false);
 
   return (
     <header>
       <nav className="container grid nav-bar">
-        <Link className="nav-bar-logo" to={links.get("home").path}>
+        <Link className="nav-bar-logo" to={links.get('home').path}>
           <img src={imageLogo} alt="Little Lemon logo" />
         </Link>
-        <button
-          className="nav-bar-hamburger"
-          type="button"
+        <button 
+          className="nav-bar-hamburger" 
+          type="button" 
           onClick={() => setNavExpanded(!navExpanded)}
         >
-          {navExpanded ? (
-            <FontAwesomeIcon icon={faXmark} size="2x" />
-          ) : (
-            <FontAwesomeIcon icon={faBars} size="2x" />
-          )}
+          {navExpanded ?
+            <FontAwesomeIcon icon={faXmark} size="2x" /> : 
+            <FontAwesomeIcon icon={faBars} size="2x" />}
         </button>
-        <ul
-          className={navExpanded ? "nav-bar-links expanded" : "nav-bar-links"}
-          onClick={() => setNavExpanded(navExpanded)}
+        <ul 
+          className={navExpanded ? 'nav-bar-links expanded' : 'nav-bar-links'} 
+          onClick={() => setNavExpanded(!navExpanded)}
         >
-          {navLinks.map(( index) => (
+          {navLinks.map((navLink, index) => 
             <li key={index}>
-              <Link
-                className={pathName === navLinks.path ? "current-location" : ""}
-                to={navLinks.path}
+              <Link 
+                className={pathName === navLink.path ? 'current-location' : ''} 
+                to={navLink.path}
               >
-                {navLinks.name}
+                {navLink.name}
               </Link>
             </li>
-          ))}
+          )}
         </ul>
       </nav>
     </header>
