0583177c1717bdca64b9e02c601b397d32fdc6df
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-11 21:06:37 -0400

    extract customers into a separate file

---
+++ src/utils/customers.js
@@ -0,0 +1,37 @@
+import customer1Image from '../components/pages/Home/assets/customer1.jpg';
+import customer2Image from '../components/pages/Home/assets/customer2.jpg';
+import customer3Image from '../components/pages/Home/assets/customer3.jpg';
+import customer4Image from '../components/pages/Home/assets/customer4.jpg';
+
+const customers = [
+    {
+      fullName: 'Celina Durham',
+      image: customer1Image,
+      rating: [1, 1, 1, 1, 0.5],
+      says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+      fullName: 'Tyree Middleton',
+      image: customer2Image,
+      rating: [1, 1, 1, 1, 1],
+      says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+      fullName: 'Louise Ramsey',
+      image: customer3Image,
+      rating: [1, 1, 1, 1, 0.5],
+      says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+      fullName: 'Jason Lang',
+      image: customer4Image,
+      rating: [1, 1, 1, 1],
+      says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+  ];
+
+  export default customers;
