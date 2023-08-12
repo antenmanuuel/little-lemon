fce694d4c4bf9c5857e555938f47beeb311ba2f6
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-11 21:30:06 -0400

    Fix routing issue for confirm booking

--- src/utils/links.js
+++ src/utils/links.js
@@ -3,7 +3,7 @@ links.set('home', {name: 'Home', path: '/', anchorable: true});
 links.set('about', {name: 'About', path: '/about', anchorable: true});
 links.set('menu', {name: 'Menu', path: '/menu', anchorable: true});
 links.set('bookings', {name: 'Reservations', path: '/bookings', anchorable: true});
-links.set('confirmBooking', {name: 'Confirm Booking', path: '/confirm-booking', anchorable: false});
+links.set('confirmedBooking', {name: 'Confirm Booking', path: '/confirm-booking', anchorable: false});
 links.set('orderOnline', {name: 'Order Online', path: '/order-online', anchorable: true});
 links.set('login', {name: 'Login', path: '/login', anchorable: true});
 
