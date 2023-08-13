378b12d03ddf613116a5bf44dc3502ffe1ae327c
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-08-12 21:15:58 -0400

    add report Vitals and setupTest.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
 import ScrollToTop from './components/layout/ScrollToTop';
+import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,3 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
